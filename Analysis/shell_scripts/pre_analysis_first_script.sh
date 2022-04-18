9cf0fc46d13b1299487fd13343f4710a70790e99
Elena Solovieva <51224356+solelena@users.noreply.github.com>
2022-04-18 10:16:47 +0900

    Update pre_analysis_first_script.sh

--- Analysis/shell_scripts/pre_analysis_first_script.sh
+++ Analysis/shell_scripts/pre_analysis_first_script.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 #
 
-source //home/user/psrelip_pipeline/psrelip.config
+CONF_FILE=/home/user/psrelip_pipeline/psrelip.config
+if [ -f "$CONF_FILE" ]
+then
+  source "$CONF_FILE"
+else
+  echo "Config file does not exist"
+  exit -1
+fi
 
-echo "PLINK2_DEV_HOME=$PLINK2_DEV_HOME" >&2
+echo "PLINK2_HOME=$PLINK2_HOME" >&2
 
 echo "TOOL_INSTALL_DIR=$TOOL_INSTALL_DIR" >&2
 PERL_PROGRAM_HOME=${TOOL_INSTALL_DIR}/program_files/perl_programs
@@ -18,24 +25,29 @@ echo "MAX_THREADS=$MAX_THREADS" >&2
 
 if [ -d "${WD}"/bed_files/ ]
 then
-  echo "Output directory 'bed_files' already exists."
-  rm -f "${WD}"/bed_files/*
+  if [ -f "${BED_INPUT_FILE}".pvar ]
+  then
+    echo "Output directory 'bed_files' already exists."
+    rm -f "${WD}"/bed_files/*
+  else
+    echo "${BED_INPUT_FILE} files do not exist"
+  fi
 else
   mkdir -p "${WD}"/bed_files
 fi
 
 if [ "${VCF_FILE_NAME##*.}" = "bcf" ]
 then
-  "${PLINK2_DEV_HOME}"/plink2 --bcf "${VCF_FILE_NAME}" --allow-extra-chr --max-alleles 2 --make-pgen --memory "$MAX_MEM_USAGE" --threads "$MAX_THREADS" --out "${BED_INPUT_FILE}"_temp
+  "${PLINK2_HOME}"/plink2 --bcf "${VCF_FILE_NAME}" --allow-extra-chr --max-alleles 2 --make-pgen --memory "$MAX_MEM_USAGE" --threads "$MAX_THREADS" --out "${BED_INPUT_FILE}"_temp
 elif [[ $VCF_FILE_NAME =~ \bcf.gz$ ]]
 then
-  "${PLINK2_DEV_HOME}"/plink2 --bcf "${VCF_FILE_NAME}" --allow-extra-chr --max-alleles 2 --make-pgen --memory "$MAX_MEM_USAGE" --threads "$MAX_THREADS" --out "${BED_INPUT_FILE}"_temp
+  "${PLINK2_HOME}"/plink2 --bcf "${VCF_FILE_NAME}" --allow-extra-chr --max-alleles 2 --make-pgen --memory "$MAX_MEM_USAGE" --threads "$MAX_THREADS" --out "${BED_INPUT_FILE}"_temp
 elif [ "${VCF_FILE_NAME##*.}" = "vcf" ]
 then
-  "${PLINK2_DEV_HOME}"/plink2 --vcf "${VCF_FILE_NAME}" --allow-extra-chr --max-alleles 2 --make-pgen --memory "$MAX_MEM_USAGE" --threads "$MAX_THREADS" --out "${BED_INPUT_FILE}"_temp
+  "${PLINK2_HOME}"/plink2 --vcf "${VCF_FILE_NAME}" --allow-extra-chr --max-alleles 2 --make-pgen --memory "$MAX_MEM_USAGE" --threads "$MAX_THREADS" --out "${BED_INPUT_FILE}"_temp
 elif [[ $VCF_FILE_NAME =~ \vcf.gz$ ]]
 then
-  "${PLINK2_DEV_HOME}"/plink2 --vcf "${VCF_FILE_NAME}" --allow-extra-chr --max-alleles 2 --make-pgen --memory "$MAX_MEM_USAGE" --threads "$MAX_THREADS" --out "${BED_INPUT_FILE}"_temp
+  "${PLINK2_HOME}"/plink2 --vcf "${VCF_FILE_NAME}" --allow-extra-chr --max-alleles 2 --make-pgen --memory "$MAX_MEM_USAGE" --threads "$MAX_THREADS" --out "${BED_INPUT_FILE}"_temp
 else
     echo "Please specify the valid VCF file in configuration file."
 fi
@@ -44,7 +56,12 @@ perl "${PERL_PROGRAM_HOME}"/ps_pl2_pvar_edit.pl "${BED_INPUT_FILE}"_temp.pvar "$
 mv "${BED_INPUT_FILE}"_temp.pgen "${BED_INPUT_FILE}".pgen
 mv "${BED_INPUT_FILE}"_temp.psam "${BED_INPUT_FILE}".psam
 
-rm -f "${BED_INPUT_FILE}"_temp.pvar
+if [ -f "${BED_INPUT_FILE}"_temp.pvar ]
+then
+  rm -f "${BED_INPUT_FILE}"_temp.pvar
+else
+  echo "${BED_INPUT_FILE}_temp.pvar file does not exist"
+fi
 
-"${PLINK2_DEV_HOME}"/plink2 --pfile "${BED_INPUT_FILE}" --allow-extra-chr --freq counts --memory "$MAX_MEM_USAGE" --threads "$MAX_THREADS" --out "${BED_INPUT_FILE}"_frc
+"${PLINK2_HOME}"/plink2 --pfile "${BED_INPUT_FILE}" --allow-extra-chr --freq counts --memory "$MAX_MEM_USAGE" --threads "$MAX_THREADS" --out "${BED_INPUT_FILE}"_frc
 
